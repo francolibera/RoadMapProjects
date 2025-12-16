2a23d4b1554b7f985439f0c11152366033810506
FR4NKK0 <francoliberaca@gmail.com>
2025-12-16 17:04:28 -0300

    feat: implement weight conversion form and temperature conversion functionality

--- BackEndProjects/Unit Converter/src/index.ts
+++ BackEndProjects/Unit Converter/src/index.ts
@@ -40,6 +40,32 @@ app.post('/convert-length', (req, res) => {
   `);
 });
 
+const temperarureRates: Record<string, number> = {
+  celsius: 0,
+  fahrenheit: 32,
+  kelvin: 273.15,
+};
+
+app.post('/convert-temperature', (req, res) => {
+  const value = parseFloat(req.body.value);
+  const fromUnit = req.body.fromUnit;
+  const toUnit = req.body.toUnit;
+  
+  const valueInCelsius = fromUnit === 'celsius' ? value :
+                        fromUnit === 'fahrenheit' ? (value - 32) * 5/9 :
+                        value - 273.15;
+
+  const result = toUnit === 'celsius' ? valueInCelsius :
+                 toUnit === 'fahrenheit' ? (valueInCelsius * 9/5) + 32 :
+                 valueInCelsius + 273.15;
+
+  res.send(`
+    <h1>Resultado</h1>
+        <p>${value} ${fromUnit} son <strong>${result.toFixed(2)} ${toUnit}</strong></p>
+        <a href="/temperature.html">Volver</a>
+  `);
+} );
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
